a60a3c128e04a8937d57354e78741ee461134b8b
DanyR2001 <danielerusso_@hotmail.it>
2023-02-11 15:28:51 +0100

    pagine web statiche

---
+++ PARN/src/main/webapp/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker();
+    $('input#input_text, textarea#textarea2').characterCounter();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
