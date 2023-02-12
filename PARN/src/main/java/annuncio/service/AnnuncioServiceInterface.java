4c63e7908929e3fc2ae6d156721bd546c7362ec7
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2023-02-12 18:42:01 +0100

    Aggiunte firme del service del sottisistema di gestione annuncio

--- PARN/src/main/java/annuncio/service/AnnuncioServiceInterface.java
+++ PARN/src/main/java/annuncio/service/AnnuncioServiceInterface.java
@@ -1,12 +1,23 @@
 package annuncio.service;
 
+import candidatura.service.CandidaturaService;
 import storage.entity.Annuncio;
+import storage.entity.Candidatura;
+import utils.ConPool;
+import utils.KeywordUtils;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface AnnuncioServiceInterface {
 
     public List<Annuncio> getAnnuncioById(int id);
+    public boolean creaAnnuncio(Annuncio annuncio);
+    public boolean modificaAnnuncio(Annuncio annuncio);
+    public boolean eliminaAnnuncio(Annuncio annuncio);
+    public boolean chiusuraAnnuncio(Annuncio annuncio);
+    public boolean aggiungiCandidatura(Annuncio annuncio, Candidatura candidatura);
+    public boolean rimuoviCandidatura(Annuncio annuncio, Candidatura candidatura);
 
-    List<Annuncio> getAnnunciByStato(String in_corso);
 }
