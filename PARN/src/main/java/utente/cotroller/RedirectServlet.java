dd3803c78de7758fe8c0d5730e12fbe711f78504
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2023-02-14 10:20:47 +0100

    Aggiunta servlet redirect

---
+++ PARN/src/main/java/utente/cotroller/RedirectServlet.java
@@ -0,0 +1,22 @@
+package utente.cotroller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "RedirectServlet", value = "/RedirectServlet")
+public class RedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String redirect = request.getParameter("redirect");
+
+        request.getRequestDispatcher("./WEB-INF/"+redirect).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
